4ab463c3afe997800e4be2f909e165147f5d7f38
ezrajames <ezra@revela.io>
2022-07-29 13:00:24 -0700

    feat: Add pointnet implementation

--- pointcloud/model.py
+++ pointcloud/model.py
@@ -35,7 +35,7 @@ class TNet(torch.nn.Module):
         x = self.relu(self.batch_norm3(self.conv3(x)))
 
         x = self.max_pooling(x)
-        x = self.view(-1, 1024)
+        x = x.view(-1, 1024)
 
         x = self.relu(self.batch_norm4(self.linear1(x)))
         x = self.relu(self.batch_norm5(self.linear2(x)))
@@ -57,3 +57,120 @@ class TNet(torch.nn.Module):
         x = x.view(-1, 3, 3)
 
         return x
+
+
+class TNetFeatureTransformer(torch.nn.Module):
+    """
+    T-Net for Feature Transform.
+    """
+
+    def __init__(self, num_points: int = 2500, global_feature: bool = True) -> None:
+        super(TNetFeatureTransformer, self).__init__()
+        self.num_points = num_points
+        self.global_feature = global_feature
+
+        self.tnet = TNet(num_points=num_points)
+
+        self.conv1 = torch.nn.Conv1d(3, 64, 1)
+        self.conv2 = torch.nn.Conv1d(64, 128, 1)
+        self.conv3 = torch.nn.Conv1d(128, 1024, 1)
+
+        self.batch_norm1 = torch.nn.BatchNorm1d(64)
+        self.batch_norm2 = torch.nn.BatchNorm1d(128)
+        self.batch_norm3 = torch.nn.BatchNorm1d(1024)
+
+        self.max_pooling = torch.nn.MaxPool1d(num_points)
+        self.relu = torch.nn.ReLU()
+
+    def forward(self, x):
+        batch_size = x.size()[0]
+        transformer = self.tnet(x)
+
+        x = x.transpose(2, 1)
+        x = torch.bmm(x, transformer)
+
+        x = x.transpose(2, 1)
+        x = self.relu(self.batch_norm1(self.conv1(x)))
+        point_feature = x
+
+        x = self.relu(self.batch_norm2(self.conv2(x)))
+
+        x = self.max_pooling(self.batch_norm3(self.conv3(x)))
+
+        x = x.view(-1, 1024)
+        if self.global_feature:
+            return x, transformer
+        else:
+            x = x.view(-1, 1024, 1).repeat(1, 1, self.num_points)
+            return torch.cat([x, point_feature], 1), transformer
+
+
+class PointNetClassifier(torch.nn.Module):
+    """
+    Network for Classification: 512, 156, K.
+    """
+
+    def __init__(self, num_points: int = 2500, k: int = 2) -> None:
+        super(PointNetClassifier, self).__init__()
+        self.num_points = num_points
+
+        self.feature_tnet = TNetFeatureTransformer(num_points)
+
+        self.linear1 = torch.nn.Linear(1024, 512)
+        self.linear2 = torch.nn.Linear(512, 256)
+        self.linear3 = torch.nn.Linear(256, k)
+
+        self.batch_norm1 = torch.nn.BatchNorm1d(512)
+        self.batch_norm2 = torch.nn.BatchNorm1d(256)
+
+        self.relu = torch.nn.ReLU()
+        self.softmax = torch.nn.LogSoftmax(dim=-1)
+
+    def forward(self, x):
+        x, transformer = self.feature_tnet(x)
+
+        x = self.relu(self.batch_norm1(self.linear1(x)))
+        x = self.relu(self.batch_norm2(self.linear2(x)))
+
+        x = self.linear3(x)
+        return self.softmax(x), transformer
+
+
+class PointNetSegmentor(torch.nn.Module):
+    """
+    Network for segmentation.
+    """
+
+    def __init__(self, num_points: int = 2500, k: int = 2) -> None:
+        super(PointNetSegmentor, self).__init__()
+        self.num_points = num_points
+        self.k = k
+
+        self.feature_tnet = TNetFeatureTransformer(num_points, global_feature=False)
+
+        self.conv1 = torch.nn.Conv1d(1088, 512, 1)
+        self.conv2 = torch.nn.Conv1d(512, 256, 1)
+        self.conv3 = torch.nn.Conv1d(256, 128, 1)
+        self.conv4 = torch.nn.Conv1d(128, self.k, 1)
+
+        self.batch_norm1 = torch.nn.BatchNorm1d(512)
+        self.batch_norm2 = torch.nn.BatchNorm1d(256)
+        self.batch_norm3 = torch.nn.BatchNorm1d(128)
+
+        self.log_softmax = torch.nn.LogSoftmax(dim=-1)
+        self.relu = torch.nn.ReLU()
+
+    def forward(self, x):
+        batchsize = x.size()[0]
+        x, transformer = self.feature_tnet(x)
+
+        x = self.relu(self.batch_norm1(self.conv1(x)))
+        x = self.relu(self.batch_norm2(self.conv2(x)))
+        x = self.relu(self.batch_norm3(self.conv3(x)))
+
+        x = self.conv4(x)
+        x = x.transpose(2, 1).contiguous()
+        x = self.log_softmax(x.view(-1, self.k))
+        x = x.view(batchsize, self.num_points, self.k)
+
+        return x, transformer
---
+++ tests/test_pointnet.py
@@ -0,0 +1,51 @@
+import torch
+
+from pointcloud.model import (
+    PointNetClassifier,
+    PointNetSegmentor,
+    TNet,
+    TNetFeatureTransformer,
+)
+
+
+def test_tnet_dim() -> None:
+    """
+    Test that TNet model has output matching expected dimensions.
+    """
+    sim_data = torch.autograd.Variable(torch.rand(32, 3, 2500))
+    trans = TNet()
+    out = trans(sim_data)
+    actual = out.size()
+    expected = [32, 3, 3]
+    assert len(actual) == len(expected)
+    assert all([a == b for a, b in zip(actual, expected)])
+
+
+def test_feature_transform_dim() -> None:
+    """
+    Test that feature transform model has expected output dimensions.
+    """
+    sim_data = torch.autograd.Variable(torch.rand(32, 3, 2500))
+
+    point_feature = TNetFeatureTransformer()
+    out, _ = point_feature(sim_data)
+    assert all([a == b for a, b in zip(out.size(), [32, 1024])])
+
+    point_feature = TNetFeatureTransformer(global_feature=False)
+    out, _ = point_feature(sim_data)
+    assert all([a == b for a, b in zip(out.size(), [32, 1088, 2500])])
+
+    classifier = PointNetClassifier(k=5)
+    out, _ = classifier(sim_data)
+    assert all([a == b for a, b in zip(out.size(), [32, 5])])
+
+
+def test_segmentor_dim() -> None:
+    """
+    Test that the segmentation model has expected output dimensions.
+    """
+    sim_data = torch.autograd.Variable(torch.rand(32, 3, 2500))
+
+    segmentor = PointNetSegmentor(k=3)
+    out, _ = segmentor(sim_data)
+    assert all([a == b for a, b in zip(out.size(), [32, 2500, 3])])
